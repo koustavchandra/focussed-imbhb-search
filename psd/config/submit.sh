2ed46f1fd2e52a79e7b0516514b7aaa1358566cc
koustav.chandra <koustav.chandra@ligo.org>
2020-09-21 19:45:15 -0700

    Moved Files

--- psd/config/submit.sh
+++ psd/config/submit.sh
@@ -1,23 +1,12 @@
-LIGO_USERNAME=${1}
-
-if test x${LIGO_USERNAME} == "x" ; then
-  echo "Error: user name not given."
-  echo
-  echo "Usage: ${0} ANALYSIS albert.einstein"
-  echo
-  echo "where albert.einstein is your LIGO.ORG username."
-  exit 1
-fi
-
-ligo-proxy-init -p $LIGO_USERNAME
+ligo-proxy-init koustav.chandra
 
 source /cvmfs/oasis.opensciencegrid.org/ligo/sw/pycbc/x86_64_rhel_7/virtualenv/pycbc-v1.16.4/bin/activate
 WORKFLOW_NAME=psd-estimation-O3-HLV-Nov24
 GPSSTART=1258642818 # 24th Nov 2019 15:00: 00 UTC
 GPSEND=1258671618 # 24th Nov 2019 23:00:00 UTC
-CONFIG_URL='https://git.ligo.org/koustav.chandra/focussed-imbhb-search/-/raw/master/psd/config/'
+CONFIG_URL='/home/koustav.chandra/o3_dev_ops/psds'
 
-ecp-cookie-init LIGO.ORG https://git.ligo.org/users/auth/shibboleth/callback $LIGO_USERNAME
+ecp-cookie-init LIGO.ORG https://git.ligo.org/users/auth/shibboleth/callback koustav.chandra
 
 
 pycbc_make_psd_estimation_workflow \
@@ -29,3 +18,5 @@ pycbc_make_psd_estimation_workflow \
         ${CONFIG_URL}/gps_times.ini \
     --config-overrides workflow:start-time:${GPSSTART} workflow:end-time:${GPSEND} \
       results_page:output-path:"/home/${USER}/public_html/o3/psds/hlv/c01/" \
+
+
