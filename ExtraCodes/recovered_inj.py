f9e15ceef85fcb1d1d5a876fb532fc31ebb6156e
Koustav Chandra <koustav.chandra@ligo.org>
2021-01-13 05:19:13 -0600

    Update recovered_inj.py

--- ExtraCodes/recovered_inj.py
+++ ExtraCodes/recovered_inj.py
@@ -20,6 +20,8 @@ parser.add_argument('--chieff',
                     help='Effective spin', type=float, default=0.)
 parser.add_argument('--chip', 
                     help='Effective precession', type=float, default=0.)
+parser.add_argument('--mass-bin',
+                    type=int, default=0)
 parser.add_argument('--ifar', 
                     help='Exclusive IFAR threshold', type=float, default=2.94)
 parser.add_argument('--output-file', type=str, default='try.csv',
@@ -34,6 +36,7 @@ mratio = float(arguments['mratio'])
 chi_eff = float(arguments['chieff'])
 chi_p = float(arguments['chip'])
 ifar = float(arguments['ifar'])
+massbin = int(arguments['mass_bin'])
 output_file = str(arguments['output_file'])
 
 logging.info("Injections parameters requested: Total-Mass {}, chip {}, chieff {}, mass-ratio {}".format(total_mass, chi_p, chi_eff, mratio))
@@ -51,9 +54,10 @@ rslt_df = rslt_df[rslt_df.chip <= (chi_p + 0.1)]
 
 rslt_df = rslt_df[rslt_df.chieff >= (chi_eff - 0.1)]
 rslt_df = rslt_df[rslt_df.chieff <= (chi_eff + 0.1)]
+rslt_df['massbin'] = massbin 
 
 logging.info('Writing to file {}'.format(output_file))
-rslt_df.to_csv(output_file)
+rslt_df.to_csv(output_file, index = False)
 rslt_df.dropna()
 rslt_df = rslt_df[rslt_df['Exc. IFAR'] >= ifar]
 
