48b697e7d2a0b6bfa47b3ff28a78659154d35acf
Koustav Chandra <koustav.chandra@ligo.org>
2021-05-18 15:54:38 +0000

    Update table_iii.py

--- rate_calculation_codes/common/table_iii.py
+++ rate_calculation_codes/common/table_iii.py
@@ -3,23 +3,10 @@ import numpy, pandas as pd
 file = 'combined_pipelines.csv'
 df = pd.read_csv(file)
 
-df['cWB_VT_sen'] = df['cWB_N_rec']/df['N_tot']*df['VT_tot']
-df['PyCBC_VT_sen'] = df['PyCBC_N_rec']/df['N_tot']*df['VT_tot']
-df['GstLAL_VT_sen'] = df['GstLAL_N_rec']/df['N_tot']*df['VT_tot']
-
-
-df2 = df[['M_tot', 'q', 'chieff', 
-          'chip', 'SIM_ID', 
-          'z', 'N_tot', 'VT_tot' , 
-          'N_rec' , 'PyCBC_N_rec', 
-          'GstLAL_N_rec', 'VT_sen',
-          'GstLAL_VT_sen', 'PyCBC_VT_sen', 
-          'cWB_VT_sen']]
-
-df['R'] = df['R_n_0']
-df['R'][6] = df['R_n_1'][6]
-drop_bins = [0, 25, 30]
-df2 = df.drop(drop_bins)
+def nsf(num):
+    """2-Significant Figures"""
+    #print(float(numstr))
+    return ("%.2g" % num)
 
 def f2(x):
   if x > 1 and x != 1.5:
@@ -29,10 +16,20 @@ def f2(x):
   else:
     return "1"
 
-df2 = df2.round(2)
-headers = ['M_tot', 'q', 'chieff', 'chip', 'SIM_ID', 'z', 'VT_sen', 'R'] 
+def f3(x):
+  return ("%d" % x)
+
+def f4(x):
+  if x == 0.00:
+    return ("%d" % x)
+  else:
+    return ("%.2g" % x)  
+
+df['mtotal'] = df['mass1_source'] + df['mass2_source']    
+df['q'] = df['mass1_source'] / df['mass2_source']
+
+headers = ['mtotal', 'q', 'chi_eff', 'chi_p', 'sim_id', 'max_redshift', 'VT_sen', 'R'] 
+df2 = df.round(2)
 print(df2.to_latex(index=False, 
                    columns=headers,
-                   formatters={'q':f2}))
-
-print(df2[headers].to_markdown(index=False))
+                   formatters={'mtotal': f3, 'chi_eff' : f4, 'chi_p' : f4, 'q':f2, 'VT_sen' : nsf, 'R':nsf}))
